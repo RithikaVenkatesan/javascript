aa0e14555b3adaa40833e409d83453b715bd76c9
RithikaVenkatesan <149246871+RithikaVenkatesan@users.noreply.github.com>
2024-08-24 00:51:56 +0530

    Create AreaOfTriangle.js

---
+++ AreaOfTriangle.js
@@ -0,0 +1,5 @@
+var base=prompt("enter base value")
+var height=prompt("enter height value")
+var area=(base * height)/2;
+console.log('base:' +base +'height:' +height+'area:'+area);
+VM554:4 base:3height:4area:6
